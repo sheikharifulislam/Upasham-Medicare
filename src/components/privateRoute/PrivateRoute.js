40c6b0568557a3e13648dd56f0e5271c1bd3a278
sheikh ariful <sheikharifulislam770@gmail.com>
2021-10-19 23:45:24 +0600

    project work  end

--- src/components/privateRoute/PrivateRoute.js
+++ src/components/privateRoute/PrivateRoute.js
@@ -3,7 +3,11 @@ import { Redirect, Route } from 'react-router';
 import useAuth from '../../customHook/useAuth';
 
 const PrivateRoute = ({children,...rest}) => {
-    const {user} = useAuth();
+    const {user, isLoading} = useAuth();
+
+    if(isLoading) {
+        return ;
+    }
     return (      
      
             <Route 
